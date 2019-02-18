776df4349cf602ba4057199f4fa72fd49486e118
Marinko <marinkospasojevic@gmail.com>
2019-02-18 19:49:13 +0100

    added support for .NET Core 2.2

---
+++ GlobalErrorHandling-Start/GlobalErrorHandling/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoggerService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalErrorHandling.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private ILoggerManager _logger;
+
+        public ValuesController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ GlobalErrorHandling-Start/GlobalErrorHandling/DataManager.cs
@@ -0,0 +1,19 @@
+﻿using GlobalErrorHandling.Models;
+using System.Collections.Generic;
+
+namespace GlobalErrorHandling
+{
+    public class DataManager
+    {
+        public static List<Student> GetAllStudents()
+        {
+            return new List<Student>
+            {
+                new Student { Name="Joe", LastName="Doe", Age=35, Gender="Male"},
+                new Student { Name="Jane", LastName="Doe", Age=25, Gender="Female"},
+                new Student { Name="Mick", LastName="Simon", Age=32, Gender="Male"},
+                new Student { Name="Sandra", LastName="Summer", Age=30, Gender="Female"},
+            };
+        }
+    }
+}
