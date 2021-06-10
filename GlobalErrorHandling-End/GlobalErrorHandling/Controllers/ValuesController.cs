2aaf4642bb62a9fecb2bacaad1658d2d968990dc
Marinko <marinkospasojevic@gmail.com>
2021-06-10 12:32:42 +0200

    Custom error messages.

--- GlobalErrorHandling-End/GlobalErrorHandling/Controllers/ValuesController.cs
+++ GlobalErrorHandling-End/GlobalErrorHandling/Controllers/ValuesController.cs
@@ -4,29 +4,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GlobalErrorHandling.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase
-    {
-        private ILoggerManager _logger;
-        public ValuesController(ILoggerManager logger)
-        {
-            _logger = logger;
-        }
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ValuesController : ControllerBase
+	{
+		private ILoggerManager _logger;
+		public ValuesController(ILoggerManager logger)
+		{
+			_logger = logger;
+		}
 
-        [HttpGet]
-        public IActionResult Get()
-        {
+		[HttpGet]
+		public IActionResult Get()
+		{
+			_logger.LogInfo("Fetching all the Students from the storage");
 
-            _logger.LogInfo("Fetching all the Students from the storage");
+			var students = DataManager.GetAllStudents(); //simulation for the data base access
 
-            var students = DataManager.GetAllStudents(); //simulation for the data base access
+			throw new AccessViolationException("Violation Exception while accessing the resource.");
 
-            throw new Exception("Exception while fetching all the students from the storage.");
+			_logger.LogInfo($"Returning {students.Count} students.");
 
-            _logger.LogInfo($"Returning {students.Count} students.");
-
-            return Ok(students);
-        }
-    }
+			return Ok(students);
+		}
+	}
 }
--- GlobalErrorHandling-End/GlobalErrorHandling/CustomExceptionMiddleware/ExceptionMiddleware.cs
+++ GlobalErrorHandling-End/GlobalErrorHandling/CustomExceptionMiddleware/ExceptionMiddleware.cs
@@ -7,40 +7,51 @@ using System.Threading.Tasks;
 
 namespace GlobalErrorHandling.CustomExceptionMiddleware
 {
-    public class ExceptionMiddleware
-    {
-        private readonly RequestDelegate _next;
-        private readonly ILoggerManager _logger;
+	public class ExceptionMiddleware
+	{
+		private readonly RequestDelegate _next;
+		private readonly ILoggerManager _logger;
 
-        public ExceptionMiddleware(RequestDelegate next, ILoggerManager logger)
-        {
-            _logger = logger;
-            _next = next;
-        }
+		public ExceptionMiddleware(RequestDelegate next, ILoggerManager logger)
+		{
+			_logger = logger;
+			_next = next;
+		}
 
-        public async Task InvokeAsync(HttpContext httpContext)
-        {
-            try
-            {
-                await _next(httpContext);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Something went wrong: {ex}");
-                await HandleExceptionAsync(httpContext, ex);
-            }
-        }
+		public async Task InvokeAsync(HttpContext httpContext)
+		{
+			try
+			{
+				await _next(httpContext);
+			}
+			catch (AccessViolationException avEx)
+			{
+				_logger.LogError($"A new violation exception has been thrown: {avEx}");
+				await HandleExceptionAsync(httpContext, avEx);
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError($"Something went wrong: {ex}");
+				await HandleExceptionAsync(httpContext, ex);
+			}
+		}
 
-        private Task HandleExceptionAsync(HttpContext context, Exception exception)
-        {
-            context.Response.ContentType = "application/json";
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+		private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+		{
+			context.Response.ContentType = "application/json";
+			context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-            return context.Response.WriteAsync(new ErrorDetails()
-            {
-                StatusCode = context.Response.StatusCode,
-                Message = "Internal Server Error from the custom middleware."
-            }.ToString());
-        }
-    }
+			var message = exception switch
+			{
+				AccessViolationException =>  "Access violation error from the custom middleware",
+				_ => "Internal Server Error from the custom middleware."
+			};
+
+			await context.Response.WriteAsync(new ErrorDetails()
+			{
+				StatusCode = context.Response.StatusCode,
+				Message = message
+			}.ToString());
+		}
+	}
 }
